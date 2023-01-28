5127218cdf6f417fc88b6af7d65c90742f7f8fd9
hasankarim18 <hallysust@gmail.com>
2023-01-28 18:02:02 +0600

    navigation bar using reactstrap and modified to the functional component

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState} from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+
+
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = ()=> {
+      setIsOpen((prev)=> !prev)
+  }
+
   return (
-    <div>Navigation</div>
+     <div className="bg-dark">
+        <Navbar
+          className="container  border-start border-danger-subtle border-end"
+          color="dark"
+          dark
+          expand="md"
+        >
+          <NavbarBrand href="/">Foodie</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink target="_blank" href="https://github.com/hasankarim18/foodie">
+                  Project Github Link
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
   )
 }
 

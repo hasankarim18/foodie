cb882ffe3dbf34bc59e5dace5c0f916b3bab8433
hasankarim18 <hallysust@gmail.com>
2023-02-02 21:07:47 +0600

    updated from reactrouter 5 to 6

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,22 +3,18 @@ import Menu from '../pages/Menu'
 import Contact from '../pages/Contact'
 import About from '../pages/About'
 import Home from '../pages/Home';
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div>
-      <Switch>
-        <Route exact path="/">
-          <Redirect exact to="/home" />
-        </Route>
-        <Route exact path="/about" render={()=> { return <About />}} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/menu" >
-          <Menu />
-        </Route>
-        <Route exact path="/home" component={Home} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/about" element={<About />} />;
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import ContactForm from "../body/ContactForm";
 
 const Contact = () => {
   return (
     <div className="container ">
       <h1>This is Contact page</h1>
+      <div className="form_container">
+        <ContactForm />
+      </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
